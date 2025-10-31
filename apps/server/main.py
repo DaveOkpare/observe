ebe142b63e2558a4f20223796568846f1f22311f
daveokpare <okparedave@gmail.com>
2025-10-31 02:38:09 +0100

    feat: implement trace insertion endpoint and database schema for spans

--- apps/server/main.py
+++ apps/server/main.py
@@ -1,11 +1,27 @@
-import json
-from fastapi import FastAPI, Request
-from .models import TraceRequest
+from asyncpg import Pool
+from fastapi import Depends, FastAPI
+
+from db.connection import get_db
+from utils import serialize_spans
+from models import TraceRequest
 
 app = FastAPI()
 
 
 @app.post("/v1/traces")
-async def get_traces(request: Request):
-    print(json.dumps(await request.json(), indent=2))
+async def insert_traces(request: TraceRequest, db: Pool = Depends(get_db)):
+    async with db.acquire() as conn:
+        spans_data = serialize_spans(request)
+        columns = [
+            "trace_id",
+            "span_id",
+            "parent_span_id",
+            "name",
+            "start_time_unix_nano",
+            "end_time_unix_nano",
+            "kind",
+            "attributes",
+            "resource_attributes",
+        ]
+        await conn.copy_records_to_table("spans", records=spans_data, columns=columns)
     return {"partialSuccess": {}}
